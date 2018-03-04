b1783813a7dc3712b37e50aef7ab26ad945414e8
baishusama <baishusama@outlook.com>
2018-03-04 12:13:02 +0800

    Update adapters.js for promises-aplus-tests.

--- adapters.js
+++ adapters.js
@@ -1,8 +1,35 @@
 var Promise = require('./promise');
 
-var fulfilled;
-var rejected;
+var fulfilled = function (value) {
+    return new Promise(function (resolve) {
+        resolve(value);
+    });
+};
+var rejected = function (reason) {
+    return new Promise(function (resolve, reject) {
+        reject(reason);
+    });
+};
+var pending = function () {
+    var promise = new Promise(function (resolve, reject) {
+    });
+    var fulfill = function (value) {
+        promise.resolve(value);
+    };
+    var reject = function (err) {
+        promise.reject(err);
+    };
+    return {
+        promise: promise,
+        resolve: fulfill,
+        reject: reject
+    };
+};
 
-module.exports.fulfilled = fulfilled;
-module.exports.rejected = rejected;
-// module.exports.pending = pending;
+var adapter = {
+    resolved: fulfilled,
+    rejected: rejected,
+    deferred: pending
+};
+
+module.exports = adapter;
